a085590c10c6f1d6d776a94b2ef1002579198646
Alan Garcia <a2041002@correo.uia.mx>
2020-12-05 22:06:46 -0600

    se hacen pruebas y arreglos de agendar.

--- autoLavado.c
+++ autoLavado.c
@@ -19,6 +19,7 @@
 #define TAM_Lista 100
 
 /** @brief se declaran las estructuras de datos */
+//clientes
 typedef struct Cliente
 {
     int numId;
@@ -30,9 +31,26 @@ typedef struct Cliente
     short int tipo;
     short int servicios;
 } typeCliente;
+//Citas
+typedef struct citas
+{
+    // los días del mes para hacer la cita.
+    int dia;
+    // hora de la cita en 24 horas. Es un arreglo porque maximo habrá 5 citas por día.
+    int hora[5];
+    //id del cliente que hara cita.  Es un arreglo porque maximo habrá 5 clientes por día.
+    int idCliente[5];
+    //cuantas citas hay registradas en todo el día.
+    int numCitas;
+    //cantidad final que será cobrada por cita. Cuando cobremos el total de dinero será sumado aca. Es un arreglo porque maximo habrá 5 citas por día.
+    int monto[5];
+} typeCitas;
 
 /** @brief Se declaran las variables globales */
+//Clientes
 typeCliente regClientes[TAM_Lista];
+//Citas
+typeCitas regCitas[5];
 
 /** @brief Prototipos de funcion */
 void mostrarMenuP(void);
@@ -46,7 +64,11 @@ int asignarId(void);
 void eliminarCliente(void);
 void editarCliente(void);
 //citas
-
+void menuCitas(void);
+void hacerCita(void);
+void mostrarCitas(void);
+//extras
+int verificarId(int);
 /**
  * @fn Funcion del programa principal.
  */
@@ -79,22 +101,23 @@ void mostrarMenuP(void)
         switch (opcionMenu)
         {
         case 1:
-            printf("Acción Catalogos\n");
+            printf("Catalogos:\n");
             menuCatalogo();
             break;
         case 2:
-            printf("Acción 2.\n");
+            printf("Clientes:\n");
             menuClientes();
             break;
         case 3:
-            printf("Acción: Agendar cita.\n");
+            printf("Agendar cita:\n");
+            menuCitas();
             break;
         case 4:
-            printf("Acción: Reportes.\n");
+            printf("Reportes:\n");
             menuReportes();
             break;
         case 5:
-            printf("Salir\n");
+            printf("Salir.\n");
             return;
             break;
         default:
@@ -107,11 +130,10 @@ void mostrarMenuP(void)
 
 void menuCatalogo(void)
 {
-
+    system("clear");
     short int opcionMenu;
     do
     {
-        system("clear");
         do
         {
             printf("Selecciona un opción del Catálogo (1-3):\n");
@@ -119,11 +141,12 @@ void menuCatalogo(void)
             printf("\t II.Regresar al menú principal.\n");
             scanf(" %hd", &opcionMenu);
             limpiarBuffer();
-        } while (opcionMenu < 1 || opcionMenu > 5);
+        } while (opcionMenu < 1 || opcionMenu > 2);
 
         switch (opcionMenu)
         {
         case 1:
+            system("clear");
             printf("Paquetes para lavado de autos. \n\n");
             printf(" 1) Espuma activa.\n");
             printf("\t a. Costo $80.00.\n");
@@ -136,7 +159,6 @@ void menuCatalogo(void)
             printf("\t b. Descripción: Espuma activa, lavado, aspirado interior, secado, súper encerado y lavado de chasis. \n\n ");
             printf("\n **NOTA**\n Agregue el servicio de aspirado de cajuela por $10.00 extra\n");
             printf(" **NOTA**\n Se incrementarán $10.00 al precio de cualquier paquete cuando los servicios sean para camionetas o pickups grandes.\n\n");
-
             break;
         case 2:
             printf(" Regresar al menú principal. \n");
@@ -239,6 +261,53 @@ void menuReportes(void)
     return;
 }
 
+void menuCitas(void)
+{
+    short int opcionMenu;
+    system("clear");
+    do
+    {
+        do
+        {
+            printf("\n\n Selecciona una opcion\n");
+            printf("1: Agendar una cita\n");
+            printf("2: Mostrar citas\n");
+            printf("3: Salir\n");
+            scanf("%hd", &opcionMenu);
+            limpiarBuffer();
+        } while (opcionMenu < 1 || opcionMenu > 3);
+
+        switch (opcionMenu)
+        {
+        case 1:
+            printf("Agendar Cita.\n");
+            hacerCita();
+            break;
+        case 2:
+            printf("Mostrar citas.\n");
+            mostrarCitas();
+            break;
+        case 3:
+            printf("Regresar menú principal.\n");
+            return;
+            break;
+        default:
+            printf("Input Invalido");
+            return;
+        }
+    } while (opcionMenu != 3);
+    return;
+}
+/*
+*
+*
+*
+*
+*
+*
+*
+*
+*/
 void mostrarClientes(void)
 {
     int numId = asignarId();
@@ -292,7 +361,7 @@ void agregarCliente(void)
         system("clear");
         do
         {
-            printf("Ingresa el tipo vehículo\n\t1 = automóvil\n\t2 = Camioneta\n\t3= Camión\n");
+            printf("Ingresa el tipo vehículo\n\t1 = automóvil\n\t2 = Camioneta\n\t3 = Camión\n");
             scanf("%hd", &regClientes[id].tipo);
             limpiarBuffer();
         } while (regClientes[id].tipo < 1 || regClientes[id].tipo > 3);
@@ -396,4 +465,205 @@ void editarCliente(void)
     system("clear");
     printf("En hora buena, el perfil del usuario %s ha sido modificado.\n", regClientes[id].nombreCliente);
     return;
+}
+/*
+*
+*
+*
+*
+*
+*
+*
+*
+*/
+/** 
+ * @brief Procedimiento para agendar un cita.
+ * @Param void
+ */
+void hacerCita(void)
+{
+    system("clear");
+    int diaCita = 0, horario = 0, idCliente = 0, monto = 0, camioneta = 0, opcion = 0, paquete = 0;
+    //vamos a seleccionar el dia de la cita usando el struct. Si es miercoles, jueves, viernes, etc.
+    do
+    {
+        system("clear");
+        printf("¿Que día será su cita?\n");
+        printf("\nSelecciona un dia:\n\n");
+        printf("\t\t1:Miercoles\n");
+        printf("\t\t2:Jueves\n");
+        printf("\t\t3:Viernes\n");
+        printf("\t\t4:Sabado\n");
+        printf("\t\t5:Domingo\n");
+        scanf("%d", &diaCita);
+    } while (diaCita < 1 || diaCita > 5);
+    //checar si ese día hay citas disponibles. (no puede haber más de 5 citas por día.)
+    if (regCitas[diaCita].numCitas >= 5)
+    {
+        printf("Lo siento no hay espacio disponible ese día (%d/5)", regCitas[diaCita].numCitas);
+        return;
+    }
+    else
+    {
+        printf("\nTu cita será el día: %d. Hay %d disponibles ese día.\n", diaCita, 5 - regCitas[diaCita].numCitas);
+    }
+    //checar disponibilidad en horarios.
+    do
+    {
+        printf("Introduce el horario  de la cita de 10 de la mañana a 15 de la tarde. (en 24 horas)\n");
+        scanf("%d", &horario);
+        if (horario < 10 || horario > 15)
+        {
+            printf("El horario es invalido. Intenta con otro horario.\n");
+        }
+        else
+        {
+            // probar citas regCitas[diaCita].hora[1] = 10;
+            //con este for checamos horarios disponibles. i llega hasta el 5 porque solo tenemos 5 citas por día. checa la hora de cada registro durante el día.
+            for (int i = 0; i < 5; i++)
+            {
+                if (horario == regCitas[diaCita].hora[i])
+                {
+                    printf("Lo siento este espacio ya está reservado. Intenta con otro horario.\n");
+                    //le damos un horario invalido para evitar que se brinque el while.
+                    horario = 25;
+                }
+            }
+        }
+
+    } while (horario < 10 || horario > 15);
+    //meter lo del cliente.   mejor esto lo dejamos hasta el final porque hay que conectarlo con clientes.c
+
+    //Cobrar Monto. Hay que poner los precios y los paquetes y que el usuario eliga uno paquete.
+    do
+    {
+        printf("Estos son los paquetes disponibles:\n\n");
+        printf("Favor de elgir una opcion\n");
+        printf("1.espuma activa 80$\n");
+        printf("2.cera liquida 90$\n");
+        printf("3.premium 99$\n");
+        scanf("%d", &paquete);
+    } while (paquete < 1 || paquete > 3);
+
+    if (paquete == 1)
+    {
+        monto += 80;
+    }
+    else if (paquete == 2)
+    {
+        monto += 90;
+    }
+    else if (paquete == 3)
+    {
+        monto += 99;
+    }
+
+    do
+    {
+        system("clear");
+        mostrarClientes();
+        printf("Porfavor ingresa el número identificador (Id) del usuario que deseas agendar.\n");
+        printf("Si no tiene un usuario creado ingrese 0 para salir.\n");
+        scanf("%d", &idCliente);
+
+        if (idCliente == 0)
+        {
+            return;
+        }
+        //verificador de ID
+        if (verificarId(idCliente) > 0)
+        {
+            printf("\n¡Usuario encontrado!");
+            if (regClientes[idCliente].tipo > 1)
+            {
+                printf("\n Tu vehículo es una camioneta.\n");
+                camioneta = 1;
+                monto += 10;
+            }
+            else
+            {
+                printf("\nTu vehículo NO es una camioneta.\n");
+                camioneta = 0;
+            }
+        }
+        else
+        {
+            printf("\nUsuario no encontrado intente de nuevo.\n");
+            camioneta = -1;
+        }
+    } while (camioneta < 0 || camioneta > 1);
+
+    do
+    {
+        printf("\nextras:\n");
+        printf("Te aspiramos la cajuela por 10$\n");
+        printf("Si quieres este servicio elige 1, de lo contrario elige 0\n\n");
+        scanf("%d", &opcion);
+    } while (opcion < 0 || opcion > 1);
+
+    if (opcion == 1)
+    {
+        monto += 10;
+    }
+    system("clear");
+    printf("El monto total es igual a $ %d\n", monto);
+    printf("La cita ha sido agendada.\n");
+    //Salvar toda la info en al struct.
+    regClientes[idCliente].servicios++;
+    regCitas[diaCita].numCitas += 1;
+    regCitas[diaCita].idCliente[regCitas[diaCita].numCitas] = idCliente;
+    regCitas[diaCita].hora[regCitas[diaCita].numCitas] = horario;
+    regCitas[diaCita].monto[regCitas[diaCita].numCitas] = monto;
+    return;
+}
+/** 
+ * @brief Procedimiento para mostrar las.
+ * @Param void
+ */
+void mostrarCitas(void)
+{
+    int diaCita, i;
+    char dias[5][10] = {"Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
+    do
+    {
+        system("clear");
+        printf("Elige el día que quieras ver las citas\n");
+        printf("\t\t1:Miercoles\n");
+        printf("\t\t2:Jueves\n");
+        printf("\t\t3:Viernes\n");
+        printf("\t\t4:Sabado\n");
+        printf("\t\t5:Domingo\n");
+        scanf("%d", &diaCita);
+    } while (diaCita < 1 || diaCita > 5);
+
+    //usar un for para imprimir las citas por día.
+    printf("Estas son las citas del dia %s\n", dias[diaCita - 1]);
+
+    for (i = 0; i < 5; i++)
+    {
+        if (regCitas[diaCita].idCliente[i] > 0)
+        {
+            printf("ID: %d\t", regCitas[diaCita].idCliente[i]);
+            printf("horario: %d\t", regCitas[diaCita].hora[i]);
+            printf("El monto a pagar es: %d\n\n", regCitas[diaCita].monto[i]);
+        }
+    }
+}
+
+/** 
+ * @brief Función que comprueba si el id ingresado es existente.
+ * @Param el id que desea verificar. 
+ * si el usuario existe regresará el mismo id, de lo contrario regresará cero. 
+ */
+int verificarId(int idIngresado)
+{
+    for (int i = 0; i < TAM_Lista; i++)
+    {
+        if (regClientes[i].numId == idIngresado)
+        {
+            return idIngresado;
+        }
+    }
+    printf("Id no encontrado, intente de nuevo.");
+    return 0;
 }
